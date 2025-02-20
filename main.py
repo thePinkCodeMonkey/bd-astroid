b75aa6497be8b5b5464f9c3466e71b6ef0e6ed2e
Kitty Tang <thepinkcodemonkey@gmail.com>
2025-02-19 21:18:24 -0800

    Up to step 8

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -16,5 +13,6 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
 if __name__ == "__main__":
     main()
