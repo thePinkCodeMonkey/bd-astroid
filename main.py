055f1731d75bc87ad99e9f71e9a8072ee436d77d
Kitty Tang <kitty.tang@copilotai.com>
2025-04-21 11:16:01 -0700

    Put all WIP in a branch

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import pygame
 from constants import *
 
 def main():
+    FPS = 60
+
+    # Initialize the pygame library
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    # end of initialization
+
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        dt = clock.tick(FPS)
         pygame.display.flip()
 
 if __name__ == "__main__":
