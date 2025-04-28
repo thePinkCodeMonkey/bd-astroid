93ba5ca874cd72a43d964822a6ea52bcad04c4cd
Kitty Tang <thepinkcodemonkey@gmail.com>
2025-04-27 19:46:56 -0700

    Completed step 6

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     FPS = 60
@@ -12,6 +13,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     # end of initialization
 
     # Game loop
@@ -19,9 +21,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
+
         screen.fill((0, 0, 0))
+        player.draw(screen)
         dt = clock.tick(FPS)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -15,3 +15,17 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255,255,255), self.triangle(),2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
